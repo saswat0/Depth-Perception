5cf533d67e71653ad51f230060fd1d319bb4e6be
Saswat <saswatsubhajyotimallick@gmail.com>
2020-07-23 13:10:14 +0530

    fcrn code

---
+++ core/fcrn.py
@@ -0,0 +1,29 @@
+from .network import Network
+
+class ResNet50UpProj(Network):
+    def setup(self):
+        (self.feed('data')
+             .conv(7, 7, 64, 2, 2, relu=False, name='conv1')
+             .batch_normalization(relu=True, name='bn_conv1')
+             .max_pool(3, 3, 2, 2, name='pool1')
+             .conv(1, 1, 256, 1, 1, biased=False, relu=False, name='res2a_branch1')
+             .batch_normalization(name='bn2a_branch1'))
+
+        (self.feed('pool1')
+             .conv(1, 1, 64, 1, 1, biased=False, relu=False, name='res2a_branch2a')
+             .batch_normalization(relu=True, name='bn2a_branch2a')
+             .conv(3, 3, 64, 1, 1, biased=False, relu=False, name='res2a_branch2b')
+             .batch_normalization(relu=True, name='bn2a_branch2b')
+             .conv(1, 1, 256, 1, 1, biased=False, relu=False, name='res2a_branch2c')
+             .batch_normalization(name='bn2a_branch2c'))
+
+        (self.feed('bn2a_branch1',
+                   'bn2a_branch2c')
+             .add(name='res2a')
+             .relu(name='res2a_relu')
+             .conv(1, 1, 64, 1, 1, biased=False, relu=False, name='res2b_branch2a')
+             .batch_normalization(relu=True, name='bn2b_branch2a')
+             .conv(3, 3, 64, 1, 1, biased=False, relu=False, name='res2b_branch2b')
+             .batch_normalization(relu=True, name='bn2b_branch2b')
+             .conv(1, 1, 256, 1, 1, biased=False, relu=False, name='res2b_branch2c')
+             .batch_normalization(name='bn2b_branch2c'))
