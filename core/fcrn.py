d004cc2a6dbddad2ed51575b6285cc9ef6f082d1
Saswat <saswatsubhajyotimallick@gmail.com>
2020-07-23 13:11:04 +0530

    additions

--- core/fcrn.py
+++ core/fcrn.py
@@ -27,3 +27,29 @@ class ResNet50UpProj(Network):
              .batch_normalization(relu=True, name='bn2b_branch2b')
              .conv(1, 1, 256, 1, 1, biased=False, relu=False, name='res2b_branch2c')
              .batch_normalization(name='bn2b_branch2c'))
+
+        (self.feed('res2a_relu',
+                   'bn2b_branch2c')
+             .add(name='res2b')
+             .relu(name='res2b_relu')
+             .conv(1, 1, 64, 1, 1, biased=False, relu=False, name='res2c_branch2a')
+             .batch_normalization(relu=True, name='bn2c_branch2a')
+             .conv(3, 3, 64, 1, 1, biased=False, relu=False, name='res2c_branch2b')
+             .batch_normalization(relu=True, name='bn2c_branch2b')
+             .conv(1, 1, 256, 1, 1, biased=False, relu=False, name='res2c_branch2c')
+             .batch_normalization(name='bn2c_branch2c'))
+
+        (self.feed('res2b_relu',
+                   'bn2c_branch2c')
+             .add(name='res2c')
+             .relu(name='res2c_relu')
+             .conv(1, 1, 512, 2, 2, biased=False, relu=False, name='res3a_branch1')
+             .batch_normalization(name='bn3a_branch1'))
+
+        (self.feed('res2c_relu')
+             .conv(1, 1, 128, 2, 2, biased=False, relu=False, name='res3a_branch2a')
+             .batch_normalization(relu=True, name='bn3a_branch2a')
+             .conv(3, 3, 128, 1, 1, biased=False, relu=False, name='res3a_branch2b')
+             .batch_normalization(relu=True, name='bn3a_branch2b')
+             .conv(1, 1, 512, 1, 1, biased=False, relu=False, name='res3a_branch2c')
+             .batch_normalization(name='bn3a_branch2c'))
